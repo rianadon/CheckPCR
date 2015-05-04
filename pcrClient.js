1ca83b34d2bd4ea51508730033e8e20d64d950b7
19RyanA <ryanadolf123@gmail.com>
2015-05-03 20:59:02 -0700

    Added support for PCR's list view, separate area for attachments

--- pcrClient.js
+++ pcrClient.js
@@ -11,11 +11,15 @@ var offline = document.getElementById("offline");
 var remember = document.getElementById("remember");
 var oc;
 function auto(onComplete) {
-	startHandling();
 	data = JSON.parse(localStorage.getItem("data"));
 	if(data) {
-		onComplete();
+		try {
+			onComplete();
+		} catch(e) {
+			console.error("Error loading last assignments");
+		}
 	}
+	document.body.className="finished";
 	start(onComplete);
 }
 function start(onComplete){
@@ -23,12 +27,12 @@ function start(onComplete){
 	var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			loadingBar.style.display = "none";
+			if(logout) logout.className="visible";
 			if(xmlhttp.responseText == "Login") {
 				console.log("Need to log in");
 				login.className="visible";
 				up = getCookie("userPass");
-				if(up != "") {
+				if(up !== "") {
 					dologin(up);
 				}
 			} else if (xmlhttp.responseText == "Load") {
@@ -39,11 +43,11 @@ function start(onComplete){
 				handleData(xmlhttp.responseText);
 			}
 		}
-	}
+	};
 	xmlhttp.onerror = function() {
 		console.log("Connection to python server failed.");
-	}
-		
+	};
+
 	xmlhttp.open("GET","start",true);
 	xmlhttp.send();
 }
@@ -80,10 +84,10 @@ function dologin(val) {
 				handleData(xmlhttp.responseText);
 			}
 		}
-	}
+	};
 	xmlhttp.open("GET","login",true);
 	if(val && val.length>0) {
-		up = window.atob(val).split(":")
+		up = window.atob(val).split(":");
 		xmlhttp.setRequestHeader("User",up[0]);
 		xmlhttp.setRequestHeader("Pass",up[1]);
 	} else {
@@ -95,71 +99,96 @@ function dologin(val) {
 }
 var data = [];
 var classList = [];
-var handledDataShort = [];
-function startHandling() {
-	if(logout) logout.className="visible";
-	document.body.className="finished";
+function parseDateHash(element) {
+	var dateSplit = element.hash.substring(1).split('-');
+	return (new Date(+dateSplit[0], +dateSplit[1]-1, +dateSplit[2])).getTime();
+}
+function findId(element, tag, id) {
+	var elements = element.getElementsByTagName(tag);
+	for(var e=0; e<elements.length; e++) {
+		if(elements[e].id.indexOf(id)!=-1) return elements[e];
+	}
 }
 function handleData(tp) {
+	console.time("Handling data");
+	loadingBar.style.display = "none";
+	var handledDataShort = [];
 	data = [];
-	startHandling()
 	var el = document.createElement('div'); //Dummy element to use to parse html
-	el.innerHTML = tp;
-	var noprints = el.getElementsByClassName("noprint")
+	el.innerHTML = tp.replace(/\.\.\/Images\/RedTriangle\.gif/g, "");
+	var noprints = el.getElementsByClassName("noprint");
 	var classes = noprints[noprints.length-3].getElementsByTagName("label");
 	for(var c=0; c<classes.length; c++) {
 		classList.push(classes[c].innerHTML);
 	}
-	console.log(classList);
 	var tds = el.getElementsByClassName("rsContentTable")[0].getElementsByTagName("td");
+	var dates;
+	if(tds.length == 14) { //week view
+		var dateHeaders = el.getElementsByClassName("rsDateHeader");
+		dates = [];
+		for(var dh=0; dh<dateHeaders.length; dh++) {
+			dates.push(parseDateHash(dateHeaders[dh]));
+		}
+		tds = [].splice.call(tds,0, 7);
+	}
 	for(var td=0; td<tds.length; td++) {
+		//now parse each day
 		var ctd = tds[td];
-		var ta = {};
-		ta["weekend"] = hasClass(ctd, "rsSatCol") || hasClass(ctd, "rsSunCol");
-		ta["otherMonth"] = hasClass(ctd, "rsOtherMonth");
-		ta["day"] = ctd.getElementsByClassName("rsDateHeader")[0].title;
-		ta["assignments"] = new Array();
+		var day = {};
+		day.weekend = hasClass(ctd, "rsSatCol") || hasClass(ctd, "rsSunCol");
+		day.otherMonth = hasClass(ctd, "rsOtherMonth");
+		if(typeof dates !== "undefined" && dates !== null) {
+			day.day = dates[td];
+		} else {
+			day.day = parseDateHash(ctd.getElementsByClassName("rsDateHeader")[0]);
+		}
+		day.assignments = [];
 		var assignments = ctd.getElementsByClassName("rsApt rsAptSimple");
 		for(var a=0; a<assignments.length; a++) {
-			var ta2 = {};
+			var assignment = {};
 			var ca = assignments[a];
-			ta2["range"] = ca.getElementsByTagName("span")[0].innerHTML;
-			var t = ca.getElementsByTagName("span")[1]
-			ta2["title1"] = t.childNodes[0].innerHTML;
-			ed = ta2["range"].split(" - ")[1];
-			ta2["endDate"] = ed ? ed.substring(0, ed.indexOf(" ")) : null;
-			ta2["startDate"] = ta2["range"].substring(0, ta2["range"].indexOf(" "));
+			assignment.range = findId(ca, "span", "StartingOn").innerHTML;
+			var t = findId(ca, "span", "lblTitle");
+			assignment.title = t.childNodes[0].innerHTML;
+			ed = assignment.range.split(" - ")[1];
+			assignment.endDate = (new Date(ed ? ed.substring(0, ed.indexOf(" ")) : null)).getTime();
+			assignment.startDate = (new Date(assignment.range.substring(0, assignment.range.indexOf(" ")))).getTime();
 			var b = t.parentNode.parentNode.innerHTML;
-			ta2["title2"] = ca.title;
 			var bUnparsed = b.substring(b.indexOf("\n", b.indexOf("</div", b.indexOf("</div")+4)));
-			var matches = /<br\s*\/?>\s*<br\s*\/?>/.exec(bUnparsed.substring(0,100));
-			if(matches == null) {
-				ta2["body"] = urlify(bUnparsed.substring(bUnparsed.indexOf("\n", bUnparsed.indexOf("<br")+3)).replace(/\.\.\/Common\/AttachmentRender.aspx\?id=/g,"https://webappsca.pcrsoft.com/Clue/Common/AttachmentRender.aspx?id="));
-			} else {
-				ta2["body"] = urlify(bUnparsed.replace(/<br\s*\/?>\s*<br\s*\/?>/,""));
-			}
-			ta2["type"] = ca.title.substring(0, ca.title.indexOf("\n")).toLowerCase().replace("& quizzes", "");
+			var ap = attachmentify(bUnparsed);
+			assignment.attachments = ap[0]
+			assignment.body = urlify(ap[1]).replace(/^(?:\s*<br\s*\/?>)*/, "").replace(/(?:\s*<br\s*\/?>)*\s*$/, "").trim();
+			assignment.type = ca.title.substring(0, ca.title.indexOf("\n")).toLowerCase().replace("& quizzes", "");
 			var st = ca.title.substring(ca.title.indexOf("\n")+1);
-			for(var c=0; c<classList.length; c++) {
+			for(c=0; c<classList.length; c++) {
 				if (st.indexOf(classList[c]) > -1) {
-					ta2["class"] = classList[c];
+					assignment.class = classList[c];
 				}
 			}
-			ta2["shortTitle"] = st.substring(st.indexOf("\n")+1);
+			assignment.shortTitle = st.substring(st.indexOf("\n")+1);
 			var append = true;
-			var short = ta2["range"]+ta2["title1"];
+			var short = assignment.range+assignment.title;
 			if(handledDataShort.indexOf(short) == -1) {
 				handledDataShort.push(short);
-				ta["assignments"].push(ta2);
+				day.assignments.push(assignment);
 			}
 		}
-		data.push(ta);
+		data.push(day);
 	}
 	localStorage.setItem("data", JSON.stringify(data));
+	console.timeEnd("Handling data");
 	oc();
 }
+function createAttachments(attachments) {
+	if(attachments.length===0) return "";
+	var box = '<div class="attachments">';
+	for(var at=0; at<attachments.length; at++) {
+		box += '<a href="https://webappsca.pcrsoft.com/Clue/Common/AttachmentRender.aspx'+attachments[at][1]+'">'+attachments[at][0]+'</a>';
+	}
+	return box+"</div>";
+}
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-function updateMonth() {
+function updateCalendar() {
 	var overWeekends = [];
 	var today;
 	ew.className = "chart";
@@ -167,7 +196,7 @@ function updateMonth() {
 		if(hasClass(views[v], "selected")) {
 			views[v].className = views[v].className.replace(" selected", "");
 		}
-		if(views[v].innerHTML == "Month") {
+		if(views[v].innerHTML == "Calendar") {
 			views[v].className += " selected";
 		}
 	}
@@ -178,13 +207,13 @@ function updateMonth() {
 		extras[y] = [];
 	}
 	for(var p=0; p<data.length; p++) {
-		if(p%7 == 0) {
+		if(p%7 === 0) {
 			lastTr = document.createElement("tr");
 			at.appendChild(lastTr);
 		}
 		var td = document.createElement("td");
-		var sd = data[p].day.split("/");
-		td.innerHTML = "<span class='month'>"+months[+sd[0] - 1]+"</span><span class='compAll' onclick='completeAll(this)'>&#x2713;&#x2713;</span><span class='day'>"+sd[1]+"</span>";
+		var day = new Date(data[p].day);
+		td.innerHTML = "<span class='month'>"+months[day.getMonth()]+"</span><span class='compAll' onclick='completeAll(this)'>&#x2713;&#x2713;</span><span class='day'>"+day.getDate()+"</span>";
 		if (data[p].weekend) td.className += " weekend";
 		if (data[p].otherMonth) td.className += " otherMonth";
 		if (dayMatch(data[p].day, new Date())) {
@@ -194,12 +223,13 @@ function updateMonth() {
 		var cp = 0;
 		var margins = 0;
 		var o2=overWeekends.slice(0); //clone it to preserve it before we start adding to it
-		for(var a=0; a<data[p].assignments.length+(p%7==0 ? o2.length : 0); a++) {
-			if(p%7==0 && a<o2.length) {
-				var a2= o2[a];
+		for(var a=0; a<data[p].assignments.length+(p%7===0 ? o2.length : 0); a++) {
+			var a2;
+			if(p%7===0 && a<o2.length) {
+				a2= o2[a];
 				overWeekends.splice(0,1); //always first index since we are removing (index decreases by 1 every time)
 			} else {
-				var a2= data[p].assignments[a];
+				a2= data[p].assignments[a];
 			}
 			var a3 = document.createElement("div");
 			a3.className = "assignment "+a2.type;
@@ -221,19 +251,18 @@ function updateMonth() {
 				}
 			}
 			//a3.style.width = "calc("+span2+"00% + "+(11*(span2-1))+"px)";
-			a3.className += " span"+span2
+			a3.className += " span"+span2;
 			for(var x = p+1; x < p+span2; x++) {
-				//console.log("\t"+p+" - adding "+e);
 				extras[x].push(cp);
 			}
 
 			if(a2.startDate != data[p].day) a3.className += " fromWeekend";
 			a3.style.marginTop = 3+(cp-(a+margins))*(38+3)+"px";
-			margins += cp-(a+margins)
+			margins += cp-(a+margins);
 			a3.innerHTML += "<div><div><small>"+a2.class+"</small> "+a2.shortTitle+"</div></div>";
 			var ab = document.createElement("article");
 			ab.style.width = 100/span2+"%";
-			ab.innerHTML = "<div><div class='close' onclick='closeThis(this)'>&times;</div><div class='pin' onclick='pinThis(this)'>p</div><div class='done' onclick='complete(this)'>&#x2713;</div><span class='range'>"+a2.range+"</span><section><h2>"+a2.title1+"</h2>"+a2.body+"</div></section></div>";
+			ab.innerHTML = "<div><div class='close' onclick='closeThis(this)'>&times;</div><div class='pin' onclick='pinThis(this)'>p</div><div class='done' onclick='complete(this)'>&#x2713;</div><span class='range'>"+a2.range+"</span><section><h2>"+a2.title+"</h2>"+createAttachments(a2.attachments)+a2.body+"</div></section></div>";
 			a3.appendChild(ab);
 			td.appendChild(a3);
 			if(doneBodies.indexOf(describeA(a2)) > -1) complete(a3, false);
@@ -265,7 +294,7 @@ function updateList() {
 			if(doneBodies.indexOf(describeA(a2)) == -1 && intersects(data[p].day, new Date(), a2.endDate)) {
 				var a3 = document.createElement("div");
 				a3.className = "assignment";
-				a3.innerHTML = "<span onclick='complete(this)'></span><div><center class='range'>"+a2.range+"</center><h2>"+a2.title1+"</h2>"+a2.body+"</div>";
+				a3.innerHTML = "<span onclick='complete(this)'></span><div><center class='range'>"+a2.range+"</center><h2>"+a2.title+"</h2>"+createAttachments(a2.attachments)+a2.body+"</div>";
 				list.appendChild(a3);
 			}
 		}
@@ -278,7 +307,7 @@ function parents(el, c) {
 	var p = el;
 	while(true) {
 		if(hasClass(p, c) || !p) {
-			break
+			break;
 		}
 		p = p.parentNode;
 	}
@@ -297,23 +326,23 @@ document.onkeydown = function(evt) {
 		var assignments = document.getElementsByClassName("assignment");
 		for(var a=0; a<assignments.length; a++) { closeThis(assignments[a]); }
 	}
-}
+};
 function daysBetween( d1, d2 ) {
   var oneDay=1000*60*60*24;
-  
+
   var d1MS = (new Date(d1)).getTime();
   var d2MS = (new Date(d2)).getTime();
-  
+
   var dif = d2MS - d1MS;
-  return Math.max(Math.floor(dif/oneDay), 0); 
+  return Math.max(Math.floor(dif/oneDay), 0);
 }
 function intersects(d1, d2, d3) {
 	var d1D = new Date(d1);
 	var d1T = d1D.setHours(0,0,0);
-	
+
 	var d3D = new Date(d3);
 	var d3T = d3D.setHours(23,59,59);
-	
+
 	var d2T = (new Date(d2)).getTime();
 	return d1T <= d2T && d2T <= d3T;
 }
@@ -321,11 +350,11 @@ function describe(el) {
 	return el.getElementsByClassName("range")[0].innerHTML+el.getElementsByTagName("h2")[0].innerHTML;
 }
 function describeA(a) {
-	return a.range+a.title1;
+	return a.range+a.title;
 }
 function complete(el,update) {
+	var a;
 	update = typeof update === "undefined" ? true : update;
-	console.log(update);
 	var p = parents(el, "assignment");
 	var ta = describe(el.parentNode);
 	var as = document.getElementsByClassName("assignment");
@@ -336,9 +365,9 @@ function complete(el,update) {
 				p.style.maxHeight=p.clientHeight+2+"px"; //2px borders
 				p.className += " completed";
 				setTimeout(function(){p.style.maxHeight=0;},1);
-				setTimeout(function(){p.remove()},200);
+				setTimeout(function(){p.remove();},200);
 			} else {
-				for(var a=0;a<as.length;a++) {
+				for(a=0;a<as.length;a++) {
 					if(describe(as[a]) == ta) {
 						as[a].className += " completed";
 					}
@@ -350,7 +379,7 @@ function complete(el,update) {
 		}
 	} else {
 		if(update) {
-			for(var a=0;a<as.length;a++) {
+			for(a=0;a<as.length;a++) {
 				if(describe(as[a]) == ta) {
 					as[a].className = as[a].className.replace(" completed", "");
 				}
@@ -374,7 +403,7 @@ function completeAll(el) {
 		if(a[x].className.indexOf("completed") > -1) completed++;
 	}
 	var markComplete = completed/a.length < 1;
-	for(var x=0; x<a.length; x++) {
+	for(x=0; x<a.length; x++) {
 		var ic = a[x].className.indexOf("completed") > -1;
 		var c = a[x].getElementsByClassName("done")[0];
 		if(markComplete) {
@@ -396,7 +425,7 @@ function dayMatch(d1, d2) {
 	var a = new Date(d1);
 	var b = new Date(d2);
 	if(a.getDate() != b.getDate()) return false;
-	if(a.getMonth() != b.getMonth()) return false
+	if(a.getMonth() != b.getMonth()) return false;
 	if(a.getFullYear() != b.getFullYear()) return false;
 	return true;
 }
@@ -406,10 +435,23 @@ function byebye() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
 			window.close();
 		}
-	}
+	};
 	xmlhttp.open("GET","quit",true);
 	xmlhttp.send();
 }
+function attachmentify(text) {
+	var attachments = [];
+	var parsed = document.createElement('div');
+	parsed.innerHTML = text.replace(/&amp;/g, "&");
+	var as = parsed.getElementsByTagName("a");
+	for(var a=as.length-1; a>=0; a--) {
+		if(as[a].id.indexOf("Attachment") != -1) {
+			attachments.push([as[a].innerHTML, as[a].search+as[a].hash]);
+			as[a].remove();
+		}
+	}
+	return [attachments, parsed.innerHTML];
+}
 function urlify(text) {
-	return text.replace(/(https?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]+)/ig, function(str, str2, offset){return /href\s*=\s*./.test(text.substring(offset-10,offset)) ? str : '<a href="'+str+'">'+str+'</a>';})
+	return text.replace(/(https?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]+)/ig, function(str, str2, offset){return /href\s*=\s*./.test(text.substring(offset-10,offset)) ? str : '<a href="'+str+'">'+str+'</a>';});
 }
