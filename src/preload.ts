6d997a0d354f25cc1c18ae65609fdc39ef40ae84
Ryan Adolf <ryanadolf123@gmail.com>
2018-05-06 21:15:03 -0700

    More preload fixes

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,15 @@
-/**
+/*
  * Fast snippets that run before the all HTML gets loaded.
  */
-if (JSON.parse(localStorage.getItem('dark') || 'false')) document.body.classList.add('dark')
-if (JSON.parse(localStorage.getItem('showInfo') || 'false')) document.body.classList.add('showInfo')
+
+ /**
+  * Set the supplied navbar class, retrieving its last value from local storage
+  */
+function navInit(name: string): void {
+    if (JSON.parse(localStorage.getItem(name) || 'false')) document.body.classList.add(name)
+}
+
+navInit('dark')
+navInit('showInfo')
+navInit('showDone')
 document.body.setAttribute('data-view', JSON.parse(localStorage.getItem('view') || '0'))
